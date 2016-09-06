8fe71f756ed15f285af475c09c08cc6af400f67b
Administrator <Administrator@XN043798>
2016-09-06 15:58:34 +0800

    initial

---
+++ desktop/src/com/v5ent/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.v5ent.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.v5ent.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
